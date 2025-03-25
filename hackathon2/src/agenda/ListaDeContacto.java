c85fa93aa2fcc35613746a906c12b816d12e3663
Munchkin <jocelynem.cobain@gmail.com>
2025-03-25 11:29:02 -0600

    Add files via upload

---
+++ hackathon2/src/agenda/ListaDeContacto.java
@@ -0,0 +1,34 @@
+package agenda;
+
+import java.util.ArrayList;
+
+public class ListaDeContacto {
+
+    ArrayList<String> listaDeContacto;
+
+    public ListaDeContacto(ArrayList<String> listaDeContacto) {
+        this.listaDeContacto = listaDeContacto;
+    }
+
+    public void añadirContacto (String nombre, String apellido, String numero) {
+
+    }
+    public void existeContacto(String nombre, String apellido, String numero){
+
+    }
+    public void listarContactos(String nombre, String apellido, String numero){
+
+    }
+    public void buscaContacto(String nombre, String apellido, String numero){
+
+    }
+    public void eliminarContacto(String nombre, String apellido, String numero){
+
+    }
+    public void agendaLlena (String nombre, String apellido, String numero){
+
+    }
+    public void espacioLibres (String nombre, String apellido, String numero){
+
+    }
+}
---
+++ hackathon2/src/agenda/Main.java
@@ -0,0 +1,24 @@
+package agenda;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList listaDeContacto = new ArrayList();
+        int opcion;
+        Scanner scanner = new Scanner(System.in);
+        opcion = scanner.nextInt();
+
+        switch(opcion) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+
+        }
+    }
+}
